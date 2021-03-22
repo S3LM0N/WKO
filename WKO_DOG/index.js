67b145f31115258205e6636c96230c0d00d2b5dd
YeonWooKim <67813381+Hello-slkdfjsej@users.noreply.github.com>
2021-03-22 17:41:12 +0900

    Add files via upload

---
+++ WKO_DOG/index.js
@@ -0,0 +1,38 @@
+document.getElementsByTagName('Text_1')[0].style.fontSize = '6vw';
+document.getElementsByTagName('Text_2')[0].style.fontSize = '6vw';
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('.image-upload-wrap').hide();
+
+            $('.file-upload-image').attr('src', e.target.result);
+            $('.file-upload-content').show();
+
+            $('.image-title').html(input.files[0].name);
+        };
+
+        reader.readAsDataURL(input.files[0]);
+
+        init().then(() => {
+            predict();
+            graph.draw();
+        });
+    } else {
+        removeUpload();
+    }
+}
+
+function removeUpload() {
+    $('.file-upload-input').replaceWith($('.file-upload-input').clone());
+    $('.file-upload-content').hide();
+    $('.image-upload-wrap').show();
+}
+$('.image-upload-wrap').bind('dragover', function () {
+    $('.image-upload-wrap').addClass('image-dropping');
+});
+$('.image-upload-wrap').bind('dragleave', function () {
+    $('.image-upload-wrap').removeClass('image-dropping');
+});
